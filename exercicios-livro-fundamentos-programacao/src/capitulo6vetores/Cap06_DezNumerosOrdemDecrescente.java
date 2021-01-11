12ed0bfc95e5dbec1ef5e8ac56cbfb7e1b024d20
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-10 23:46:22 -0300

    Execicios capitulo 6 vetores

---
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_DezNumerosOrdemDecrescente.java
@@ -0,0 +1,37 @@
+package capitulo6vetores;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Cap06_DezNumerosOrdemDecrescente {
+
+	public static void main(String[] args) {
+		
+		//receber 10 numeros e mostralos em ordem decrescente
+		
+		Scanner t = new Scanner(System.in);
+		Integer[] numero = new Integer[10];
+		Integer auxiliar = 0;
+		
+		for(int i = 0 ; i < 10; i++) {
+			System.out.println("Informe o n° "+(i+1)+": ");
+			numero[i] = t.nextInt();
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 9; j++) {
+				if(numero[j] < numero[j+1]) {
+					auxiliar = numero[j];
+					numero[j] = numero[j+1];
+					numero[j+1] = auxiliar;
+				}
+			}
+		}
+		
+		System.out.println("***Numeros Ordenados***");
+		for(int i =0; i < 10; i++) {
+			System.out.println(numero[i]);
+		}
+		
+	}
+}
