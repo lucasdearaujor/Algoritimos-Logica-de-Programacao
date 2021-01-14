914e59942ea8f38296d2ae45f91d0536954dcc7c
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-13 21:43:49 -0300

    Exercicio 2 capitulo 5 Vetores

---
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio2_MostrarNumerosMultiplos.java
@@ -0,0 +1,44 @@
+package capitulo6vetores;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Cap06_Exercicio2_MostrarNumerosMultiplos {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner t = new Scanner(System.in);
+		Integer[] numeros = new Integer[7];
+		
+		for(int i = 0 ; i < 7; i++) {
+			System.out.println("Informe o n° "+(i+1)+": ");
+			numeros[i] = t.nextInt();
+		}
+		
+		System.out.println("*** Multiplos de 2 ***");
+		for(int i = 0; i < 7; i++) {
+			if(numeros[i]%2 == 0) {
+				System.out.println(numeros[i]);
+			}
+		}
+		
+		System.out.println("*** Multiplos de 3 ***");
+		for(int i = 0; i < 7; i++) {
+			if(numeros[i]%3 == 0) {
+				System.out.println(numeros[i]);
+			}
+		}
+		
+		System.out.println("*** Multiplos de 2 e 3 ***");
+		for(int i = 0; i < 7; i++) {
+			if(numeros[i]%2 == 0) {
+				System.out.println(numeros[i]);
+			}
+			if(numeros[i]%3 == 0) {
+				System.out.println(numeros[i]);
+			}
+		}
+		
+	}
+}
