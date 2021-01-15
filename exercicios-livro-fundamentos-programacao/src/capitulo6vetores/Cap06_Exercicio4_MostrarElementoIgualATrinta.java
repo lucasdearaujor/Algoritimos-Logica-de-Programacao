3c99121ca87d9e992f402b0970b4e2860600a5aa
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-14 22:31:13 -0300

    Exercicio 4 capitulo 5 Vetores

---
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio4_MostrarElementoIgualATrinta.java
@@ -0,0 +1,29 @@
+package capitulo6vetores;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Cap06_Exercicio4_MostrarElementoIgualATrinta {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner t = new Scanner(System.in);
+		Integer[] numeros = new Integer[15];
+		
+		for(int i = 0 ; i < 15; i++) {
+			System.out.println("Informe o n° "+(i+1)+": ");
+			numeros[i] = t.nextInt();
+		}
+		
+		for(int i = 0 ; i < 15; i++) {
+			if(numeros[i] == 30) {
+				System.out.println("Numero: "+numeros[i]+" na posição: "+i);
+			}
+		}
+		
+		
+		
+		
+	}
+}
