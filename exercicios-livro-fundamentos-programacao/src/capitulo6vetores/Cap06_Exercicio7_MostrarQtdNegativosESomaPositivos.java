4deb24e6f90898d392cb4c2e1312060ff5869a80
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-15 22:04:39 -0300

    Exercicio 4 capitulo 7 Vetores

---
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio7_MostrarQtdNegativosESomaPositivos.java
@@ -0,0 +1,33 @@
+package capitulo6vetores;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;import capitulo5estruturaderepeticao.Cap05Exerc12_MostrarQtdNumerosPrimos;
+
+public class Cap06_Exercicio7_MostrarQtdNegativosESomaPositivos {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner t = new Scanner(System.in);
+		Integer[] numeros = new Integer[10];
+		Integer qtdNegativos = 0;
+		Integer somaPositivos = 0;
+		
+		for(int i = 0 ; i < 10; i++) {
+			System.out.println("Informe o n° "+(i+1)+": ");
+			numeros[i] = t.nextInt();
+		}
+		
+		for(int i = 0 ; i < 10; i++) {
+			if(numeros[i] < 0) {
+				qtdNegativos++;
+			} else {
+				somaPositivos += numeros[i];
+			}
+		}
+		
+		System.out.println("Qtd de numeros Negativos: "+qtdNegativos);
+		System.out.println("Soma dos números positivos: "+somaPositivos);
+		
+	}
+}
