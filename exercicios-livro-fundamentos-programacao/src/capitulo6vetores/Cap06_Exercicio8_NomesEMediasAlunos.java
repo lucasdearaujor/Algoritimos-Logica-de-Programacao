07c4cc0ba3ee72f78d2a53fb8fcb1d39ce0bb958
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-17 22:34:42 -0300

    Exercicio 8 capitulo 6 Vetores

--- exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio8_NomesEMediasAlunos.java
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio8_NomesEMediasAlunos.java
@@ -10,23 +10,31 @@ public class Cap06_Exercicio8_NomesEMediasAlunos {
 		
 		Scanner t = new Scanner(System.in);
 		String[] nomes = new String[7];
-		Integer[] medias = new Integer[7];
+		Double[] medias = new Double[7];
 		String[] nomesNaoAprovados = new String[7];
-		Integer[] mediasNomesNaoAprovados = new Integer[7];
+		Double[] mediasNomesNaoAprovados = new Double[7];
 		String nomeAlunoMaiorMedia = "";
+		Double maiorMedia = 0.0;
 		
-		for(int i = 0 ; i < 7; i++) {
+		for(int i = 0 ; i < 2; i++) {
 			System.out.println("Informe o nome do aluno n° "+(i+1)+": ");
 			nomes[i] = t.next();
 			System.out.println("Informe a média do aluno n° "+(i+1)+": ");
-			medias[i] = t.nextInt();
+			medias[i] = t.nextDouble();
 		}
 		
-		for(int i = 0 ; i < 10; i++) {
+		for(int i = 0 ; i < 2; i++) {
+			if(medias[i] > maiorMedia) {
+				maiorMedia = medias[i];
+				nomeAlunoMaiorMedia = nomes[i];
+			}
+			
+			if(medias[i] < 7) {
+				System.out.println("Aluno nome: "+nomes[i]+" não aprovado, precisa tirar: "+ (10 - medias[i]) + " na prova final");
+			}
 		}
 		
-		System.out.println("Qtd de numeros Negativos: " + qtdNegativos);
-		System.out.println("Soma dos números positivos: " + somaPositivos);
+		System.out.println("Aluno com maior media: "+nomeAlunoMaiorMedia);
 		
 	}
 }
