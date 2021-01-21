db0d2fe6140cf3dbaa0a534ce867a001ed751c0e
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-20 23:03:04 -0300

    Exercicio 13 capitulo 6 Vetores

---
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio13_RelatorioDeMedias.java
@@ -0,0 +1,38 @@
+package capitulo6vetores;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import capitulo5estruturaderepeticao.Cap05Exerc12_MostrarQtdNumerosPrimos;
+
+public class Cap06_Exercicio13_RelatorioDeMedias {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner t = new Scanner(System.in);
+		String nomes[] = new String[8];
+		Double notas[] = new Double[8];
+		Double somaNotas = 0.0;
+		
+		for(int i = 0 ; i < 8; i++) {
+			System.out.println("Digite o nome do "+(i+1)+"° aluno");
+			nomes[i] = t.next();
+			System.out.println("Digite a nota do "+nomes[i]);
+			notas[i] = t.nextDouble();
+			somaNotas += notas[i];
+			
+		}
+		System.out.println("Relatórios de notas");
+		System.out.println("Aluno | Nota");
+		for(int i = 0 ; i < 8; i++) {
+			System.out.println(nomes[i] + " | "+notas[i]);
+			
+		}
+		System.out.println("A média da classe = "+ somaNotas / 8);
+		
+	}
+		
+}
