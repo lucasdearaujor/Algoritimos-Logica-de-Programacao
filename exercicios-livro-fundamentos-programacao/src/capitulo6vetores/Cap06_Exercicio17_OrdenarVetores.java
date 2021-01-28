8f1b44c2438b97926e67a3cbc2ea6947b28dd5f1
Lucas Araujo <lucasdearaujor@gmail.com>
2021-01-27 21:27:09 -0300

    Exercicio 17 Capitulo 6 Vetores

--- exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio17_OrdenarVetores.java
+++ exercicios-livro-fundamentos-programacao/src/capitulo6vetores/Cap06_Exercicio17_OrdenarVetores.java
@@ -10,6 +10,8 @@ public class Cap06_Exercicio17_OrdenarVetores {
 		Integer vetor1[] = new Integer[10];
 		Integer vetor2[] = new Integer[10];
 		Integer vetorOrdenado[] = new Integer[20];
+		int auxiliar = 0;
+		
 		System.out.println("*** Preencher 1° vetor ***");
 		for(int i = 0 ; i < 10; i++) {
 			System.out.println("Digente o "+(i+1)+"° numero: ");		
@@ -22,13 +24,27 @@ public class Cap06_Exercicio17_OrdenarVetores {
 		}
 		
 		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 10; j++) {
-				int auxiliar = 0;
-				if(vetor1[i] < vetor1[i+1]) {
-					auxiliar = vetor1[i];
-					vetor[i]
+			vetorOrdenado[i] = vetor1[i];
+		}
+		for(int i = 10; i < 20; i++) {
+			vetorOrdenado[i] = vetor2[i - 10];
+		}
+		
+		
+		for(int i = 0; i < 20; i++) {
+			for(int j = 0; j < 19; j++) {
+				if(vetorOrdenado[j] < vetorOrdenado[j+1]) {
+					auxiliar = vetorOrdenado[j];
+					vetorOrdenado[j] = vetorOrdenado[j+1];
+					vetorOrdenado[j+1] = auxiliar; 
 				}
 			}
+		}
+		
+		System.out.println("*** Vetor Completo Ordenado ***");
+		for(int i = 0; i < 20; i++) {
+			System.out.println(vetorOrdenado[i]);
+			
 		}
 		
 		
